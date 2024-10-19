27786fc89470a7a0f9013b5a87e135d616c0c1d4
Alfred <akojo2273@gmail.com>
2024-10-19 04:28:14 +0000

    "Update MySQLServer.py file"

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,7 @@ def create_database(alx_book_store):
     try:
         # Establish the connection to the MySQL server
         connection = mysql.connector.connect(
-            host='localhost',
+            host='localhost', 
             user='alfred1',
             password='1234567890'
         )
@@ -14,19 +14,19 @@ def create_database(alx_book_store):
             # Create a cursor object to execute SQL commands
             mycursor = connection.cursor()
             # Create database if it doesn't already exist
-            mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {alx_book_store}")
+            mycursor.execute(f"CREATE DATABASE IF NOT EXISTS {alx_book_store}")  
             print(f"Database '{alx_book_store}' created successfully!")
     
     except Error as e:
         print(f"Error while connecting to MySQL: {e}")
     
     finally:
-        # Close the cursor and connection if they were opened
-        if 'mycursor' in locals():  # Check if mycursor is defined
+       
+        if 'mycursor' in locals():  
             mycursor.close()
         if connection.is_connected():
             connection.close()
             print("MySQL connection is closed.")
 
 # Call the function to create the database
-create_database('alx_book_store')
+create_database('alx_book_store') 
